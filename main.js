fc567a3a1d749c44b2e40875cefba04edd0ef74b
Simren Chandan <simrenkaursoor@gmail.com>
2022-04-12 15:23:59 +0100

    added array of objects for list items and sorted list by document query selector

---
+++ main.js
@@ -0,0 +1,43 @@
+const list = document.querySelector(".list");
+
+const sort_name_button = document.querySelector(".sort-options .sort-name");
+const sort_meta_button = document.querySelector(".sort-options .sort-meta");
+const sort_age_button = document.querySelector(".sort-options .sort-age");
+
+let list_items = [
+   {
+      name: "Batman",
+      meta: "Black",
+      age: "58",
+   },
+   {
+      name: "Superman",
+      meta: "Red & Blue",
+      age: "33",
+   },
+   {
+      name: "Wonder Woman",
+      meta: "Gold, Red & Blue",
+      age: "34",
+   },
+   {
+      name: "The Flash",
+      meta: "Red & Yellow",
+      age: "24",
+   },
+   {
+      name: "Cyborg",
+      meta: "Silver",
+      age: "28",
+   },
+   {
+      name: "Green Arrow",
+      meta: "Green",
+      age: "38",
+   },
+   {
+      name: "Nightwing",
+      meta: "Blue & Black",
+      age: "30",
+   },
+];
